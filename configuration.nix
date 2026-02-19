36e7d7a8ae80cd0b1c5a7169c601719ca36baf78
Pedro <pedro@nixos-dev>
2026-02-19 16:51:30 +0100

    feat: entorno de desarrollo Node.js 22, Python 3, .NET 8, Docker

--- configuration.nix
+++ configuration.nix
@@ -27,6 +27,7 @@
     defaultGateway = "192.168.122.1";
     nameservers = [ "8.8.8.8" ];
   };
+
   # Set your time zone.
   time.timeZone = "Europe/Madrid";
 
@@ -38,15 +39,12 @@
   i18n.defaultLocale = "es_ES.UTF-8";
   console = {
   #   font = "Lat2-Terminus16";
-     keyMap = "es";
+    keyMap = "es";
   #   useXkbConfig = true; # use xkb.options in tty.
   };
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
-
-
-  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -66,10 +64,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.pedro = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
   #   packages = with pkgs; [
   #     tree
   #   ];
@@ -80,8 +78,8 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     git
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
   #   wget
   ];
 
@@ -93,10 +91,12 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
+  # List services you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
+  virtualisation.docker.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -105,7 +105,7 @@
   # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
+  # (/run/current-system). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
@@ -116,7 +116,7 @@
   # even if you've upgraded your system to a new NixOS release.
   #
   # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # so changing it will NOT upgrade your system – see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
   # to actually do that.
   #
   # This value being lower than the current NixOS release does NOT mean your system is
@@ -130,4 +130,3 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
-
